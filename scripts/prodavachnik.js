7d12c324eda0996f8a1e55800138ff6ec995fe15
dimitrovaElka <dimitrova_elka@abv.bg>
2017-10-20 10:46:11 +0300

    Stage 4, Task 2 – Notifications

--- scripts/prodavachnik.js
+++ scripts/prodavachnik.js
@@ -1,5 +1,6 @@
 function startApp() {
 
+
     showHideMenuLinks();
     showHomeView();
 
@@ -14,6 +15,17 @@ function startApp() {
     // Bind the form submit buttons
     $("#buttonLoginUser").click(loginUser);
     $("#buttonRegisterUser").click(registerUser);
+	
+    // Bind the info / error boxes
+    $("#infoBox, #errorBox").click(function() {
+		$(this).fadeOut();
+    });
+
+    // Attach AJAX "loading" event listener
+    $(document).on({
+        ajaxStart: function() { $("#loadingBox").show() },
+        ajaxStop: function() { $("#loadingBox").hide() }
+    });
 	
 	const kinveyBaseUrl = "https://mock.api.com/";
     const kinveyAppKey = "kid_rk";
@@ -42,6 +54,33 @@ function startApp() {
             $("#linkLogout").show();
         }
     }
+	
+	// added for Task 4-2
+    function showInfo(message) {
+        $('#infoBox').text(message);
+        $('#infoBox').show();
+        setTimeout(function() {
+            $('#infoBox').fadeOut();
+        }, 3000);
+    }
+
+    function showError(errorMsg) {
+        $('#errorBox').text("Error: " + errorMsg);
+        $('#errorBox').show();
+    }
+
+    function handleAjaxError(response) {
+        let errorMsg = JSON.stringify(response);
+        if (response.readyState === 0)
+            errorMsg = "Cannot connect due to network error.";
+        if (response.responseJSON && response.responseJSON.description)
+            errorMsg = response.responseJSON.description;
+        showError(errorMsg);
+    }
+
+    /* function showHomeView() {
+        showView('viewHome');
+    } */
 	
     function showHomeView() {
         showView('viewHome');
@@ -72,7 +111,8 @@ function startApp() {
             url: kinveyLoginUrl,
             headers: kinveyAuthHeaders,
             data: userData,
-            success: loginSuccess
+            success: loginSuccess,
+			error: handleAjaxError
         });
 
         function loginSuccess(userInfo) {
@@ -80,6 +120,7 @@ function startApp() {
             showHideMenuLinks();
             showHomeView();
 			listAdverts();
+			showInfo('Login successful.');
         }
     }
 
@@ -107,7 +148,8 @@ function startApp() {
             url: kinveyRegisterUrl,
             headers: kinveyAuthHeaders,
             data: userData,
-            success: registerSuccess
+            success: registerSuccess,
+			error: handleAjaxError
         });
 
         function registerSuccess(userInfo) {
@@ -116,6 +158,7 @@ function startApp() {
             showHideMenuLinks();
             showHomeView();
 			listAdverts();
+			showInfo('User registration successful.');
         }
     }
 
@@ -125,6 +168,7 @@ function startApp() {
         $('#loggedInUser').text("");
         showHideMenuLinks();
         showHomeView();
+		showInfo('Logout successful.');
     }
 	
 	    // advertisement/all
@@ -140,10 +184,12 @@ function startApp() {
             method: "GET",
             url: kinveyAdvertsUrl,
             headers: kinveyAuthHeaders,
-            success: loadAdvertsSuccess
+            success: loadAdvertsSuccess,
+			error: handleAjaxError
         });
 
         function loadAdvertsSuccess(adverts) {
+			showInfo('Advertisements loaded.');
             if (adverts.length === 0) {
                 $('#ads').text('No advertisements available.');
             } else {
